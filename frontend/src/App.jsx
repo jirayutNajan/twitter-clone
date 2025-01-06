e606f7a0379ef001641ba7a4505561acb7825652
Jirayut Najan <tonklaeiei123@gmail.com>
2025-01-06 20:52:49 +0700

    add frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom"
+
+import SignUpPage from "./pages/auth/signup/SignUpPage"
+import LoginPage from "./pages/auth/login/LoginPage"
+import HomePage from "./pages/home/HomePage"
+import Sidebar from "./components/common/Sidebar"
+import RightPanel from "./components/common/RightPanel"
+import NotificationPage from "./pages/notification/NotificationPage"
+import ProfilePage from "./pages/profile/ProfilePage"
+
+import { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import LoadingSpinner from "./components/common/LoadingSpinner"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // data: authUser if from return value
+  const { data: authUser, isLoading, error, isError } = useQuery({
+    queryKey: ['authUser'],// เอ้าไว้ refer ถึง Query ตัวนี้ ถ้า component หรือ page ไหนต้องการใช้ จะได้ไม่ต้อง query ใหม่
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+
+        if(data.error) {
+          return null;
+        }
+
+        if(!res.ok) {
+          throw new Error(data.error || "Something went wrong")
+        }
+
+        // console.log("auth user is here:", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false
+  })
+
+  if(isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    )
+  }
+
+  console.log(authUser);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex max-w-6xl mx-auto">
+      {authUser && <Sidebar />}
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" /> } />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" /> } />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" /> } />
+        <Route path="/notifications" element={authUser ? <NotificationPage /> : <Navigate to="/login" /> } />
+        <Route path="/profile/:username" element={authUser ? <ProfilePage /> : <Navigate to="/login" /> } />
+      </Routes>
+      {authUser && <RightPanel />}
+      <Toaster />
+    </div>
   )
 }
 
