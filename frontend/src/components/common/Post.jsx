d961d53a5e20361c4cd70d55b96747edea28b5ce
Jirayut Najan <tonklaeiei123@gmail.com>
2025-01-07 21:55:53 +0700

    comment on post

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -4,10 +4,13 @@ import { FaRegHeart } from "react-icons/fa";
 import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
+
 import { data, Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
@@ -52,7 +55,7 @@ const Post = ({ post }) => {
 
 				if(!res.ok) throw new Error(data.error || "Something went wrong");
 
-				return data;
+				return data;// --> updatedLikes
 			} catch (error) {
 				throw new Error(error);
 			}
@@ -78,14 +81,50 @@ const Post = ({ post }) => {
 		}
 	})
 
+	// comment
+	const { mutate: commentPost, isPending: isCommenting } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: comment })
+				});
+				const data = await res.json();
+
+				if(!res.ok) throw new Error(data.error || "Something went wrong");
+
+				return data;
+
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: (post) => {
+			setComment("");
+			// queryClient.invalidateQueries({ queryKey: ["posts"] }); 
+			queryClient.setQueryData(["posts"], (oldData) => {
+				return oldData.map(p => {
+					if(p._id === post._id) {
+						return {...p, comments: post.comments}
+					}
+					return p;
+				})
+			});
+		},
+		onError: (error) => {
+		toast.error(error.message);
+		}
+	})
+
   const postOwner = post.user;
   const isLiked = post.likes.includes(authUser._id);
 
   const isMyPost = postOwner._id == authUser._id;
 
-  const formattedDate = "1h";
-
-  const isCommenting = false;
+  const formattedDate = formatPostDate(post.createdAt);
 
   const handleDeletePost = () => {
     deletePost();
@@ -93,6 +132,8 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault();
+		if(isCommenting) return; // ไม่ให้ comment ตอนโหลด
+		commentPost();
   };
 
   const handleLikePost = () => {
