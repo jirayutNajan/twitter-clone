bc24cea2cded83b9778a6738a4dc31fcc0fec3a3
Jirayut Najan <tonklaeiei123@gmail.com>
2025-02-02 19:32:17 +0700

    fix something

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -8,7 +8,8 @@ export const generateTokenAndSetCookie = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 15*24*60*60*1000,// expire date of cookie
-    httpOnly: true,// prevent xss attacks cross-site scripting attacks
+    // httpOnly: true,// prevent xss attacks cross-site scripting attacks
+    // sameSite: "strict", //CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development"
   });
 }
